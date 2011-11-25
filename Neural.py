07d1f20799cdb6bbf735f8ee6507b7380ed2659e
Joseph Wright <joegle@gmail.com>
2011-11-25 02:23:44 -0500

    Fixed up Network.addNode(), cleaned up a little of everything; uncoupling

--- Neural.py
+++ Neural.py
@@ -30,9 +30,9 @@ def distance(a,b): #co-domain =[0..1]
 
 class Node:
     """A single 'neuron'"""
-    def __init__(self,size,memory=1000):
+    def __init__(self,size,memory=500):
         self.name   = "" #name to refer 
-        self.disc = "" #description of node
+        self.discription = "" #description of node
         self.output = [] #output layer buffer
         self.input  = [] #input layer buffer
 
@@ -43,6 +43,7 @@ class Node:
         self.size   = size #number of inputs
         self.age    = 0
         self.bias   = np.zeros([self.size+3]) #a prior prob
+        self.obias  = np.zeros([self.size+3]) #a prior prob
         self.mem    = memory # history buffer size
         self.hist   = [] # history 
         self.ohist  = [] # output history
@@ -57,12 +58,10 @@ class Node:
 
         self.chain  = [self.updateAge, self.updateHistory, self.computeOutput, self.calcBias,  self.cluster]
 
-
-
-    def makeNode(self,params):
-        self.size=params[0]
-        self.age=params[1]
-
+    def __getitem__(self,n):
+        # Don't depend on this 
+        return self.output[n]
+    
     def connect(self,layer_node_slot):
         """make a connection to this node to a (layer,node,slot)
         updates inmap"""
@@ -70,14 +69,10 @@ class Node:
             self.inmap.append(layer_node_slot)
             self.size+=1
 
-    def __getitem__(self,n):
-        # Don't depend on this 
-        return self.output[n]
-    
     def explain(self):
+        """ show the order of the readin call chain and whats going on"""
         n=1
         for function in self.chain:
-            
             print str(n)+". "+function.__name__
             print "\t"+function.__doc__+"\n"
             n+=1
@@ -97,18 +92,17 @@ class Node:
         self.calcBias()
         self.cluster()
 
-        # now compute expectations
     def updateAge(self):
         """Update the age of a node"""
         self.age+=1
 
     def computeOutput(self):
         """Compute the output vector of the node"""
+        self.output=[]
         for category in self.k:
             diff = distance(category,self.input)
-            self.output = diff
+            self.output.append(diff)
             self.ohist.append(diff)
-
  
     def cluster(self):
         """Build the categories of the node by kmeans clustering the history"""
@@ -131,13 +125,9 @@ class Node:
         """Update the bias vector of expected value for inputs"""
 
         # Condition to satisfy in order to update
-        if random.randint(0,self.mem)!=0:
-            return 0
-
-        # Actual computation
-        self.bias=array(self.hist).mean(axis=0)
-#        print self.age,self.mem,self.bias
-        return 1  
+        if random.randint(0,self.mem)==0:
+            self.bias=array(self.hist).mean(axis=0)
+            self.obias=array(self.ohist).mean(axis=0)
 
 #############################################
 ########## Layer ############################
@@ -145,15 +135,15 @@ class Node:
 
 class Layer:
     """A single layer of nodes"""
-    def __init__(self,nodenum,insize,memsize):
+    def __init__(self):
         self.level        = 0
-        self.length       = nodenum
+        self.length       = 0
         self.nodes        = []
         self.output_layer = []
-        self.avginput     = insize
-        self.banksize     = memsize
+        self.avginput     = 0
+        self.banksize     = 0
 
-        for x in range(nodenum):
+        for x in range(0):
             self.nodes.append(Node(insize,memsize))
 
 #   def stats(self):
@@ -167,7 +157,15 @@ class Layer:
         self.length += 1
 
     def metric(self):
-        """Return the dimensions of output layer; a list of the sizes of the output layer"""
+        """computer the demesion of the output_layer"""
+        self.output_layer=[]
+        for node in self.nodes:
+            self.output_layer.append(len(node.output))
+        
+    def giveConnection(self):
+        node=random.randint(0,self.length-1)
+        slot=random.randint(0,len(self.nodes[node].k))
+        return [node, slot]
 
     def addBlankNode(self,memory=1000):
         self.length+=1
@@ -190,7 +188,7 @@ class Network:
         return self.layers[n]
 
     def addLayer(self): #on top
-        self.layers.append(Layer(0,20,20))
+        self.layers.append(Layer())
         self.layers[-1].level=len(self.layers)
 
     def addNode(self,level,node):
@@ -199,9 +197,14 @@ class Network:
             level=len(self.layers)-1
         self[level].append(node)
 
+        for innput in range(node.size):
+            ns=self[level-1].giveConnection()
+            layer_node_slot = [level-1 , ns[0],ns[1]]
+            node.connect(layer_node_slot)
+
     def throwNodeNormal(self,sigma,n):
         "Throw a node to watch the 2D input, with random center and spread of sigma on gauss with n inputs"
-        self.addNode(1,Node(n,100))
+        self[1].append(Node(n,100))
         # assuming one input matrix for now
         slots=[]
         for limit in self[0][0].output.shape:
@@ -224,13 +227,13 @@ class Network:
         self[0][-1].output = matrix
         
     def pullup(self,level):
-        "Like pushup()"
-        if level!=0:
+        "pull up and process the subordinate layer with respect to level; Like pushup()"
+        if level!=0: # source layer doesn't have subordinate
             for node in self.layers[level]:
                 values=[]
-                for cn in node.inmap:
-                    sl=cn[2]
-                    values.append(self[cn[0]][cn[1]].output[sl[0]][sl[1]][sl[2]])
+                for connection in node.inmap:
+                    sl=connection[2]
+                    values.append(self[connection[0]][connection[1]].output[sl[0]][sl[1]][sl[2]])
                 node.readin2(values)
         
     def cycle(self):
